adde274907244f1a817fcd87511149e196767738
hope-dad <youngsikwon93@gmail.com>
2025-05-27 13:35:04 +0900

    제거

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  siteTitle: "Tania's Website",
-  siteUrl: 'https://tania.dev',
-  siteLogo: '/logo.png',
-  description:
-    'Software engineer and open source creator. This is my digital garden.',
-}
-
-export default config
